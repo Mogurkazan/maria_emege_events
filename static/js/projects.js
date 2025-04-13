331252bb8f7ee419bdd6cd34d13f9e5aa75c2401
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2025-04-13 16:58:04 +0200

    swiper

---
+++ static/js/projects.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+    slideShadows: false,
+  },
+});
