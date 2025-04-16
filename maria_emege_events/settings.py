eeb516dbdbb7416033a0d2a654feefe0c9b31936
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2025-04-16 11:41:45 +0200

    swiper ok en desk

--- maria_emege_events/settings.py
+++ maria_emege_events/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -113,10 +114,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -127,5 +127,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 
 # Static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+SASS_PROCESSOR_ROOT = BASE_DIR / 'static' 
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
--- static/js/projects.js
+++ static/js/projects.js
@@ -1,18 +1,32 @@
-const swiper = new Swiper(".swiper-container", {
-  slidesPerView: "auto",
-  centeredSlides: true,
-  loop: true,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  effect: "coverflow",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2.5,
-    slideShadows: false,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("✅ Swiper JS está corriendo y el DOM ya está cargado");
+
+  const swiper = new Swiper("#main-swiper", {
+    slidesPerView: 1.1,
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+      slideShadows: false,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: "auto",
+      },
+    },
+  });
 });
